6726fb6bcae26a169b78deff9254260e3c81d8d7
M. Yoga Nugraha <myoganugraha@gmail.com>
2018-02-22 23:48:10 +0800

    aaaall you can eat

--- app/src/main/java/itk/myoganugraha/ppb_minggu2/MainActivity.java
+++ app/src/main/java/itk/myoganugraha/ppb_minggu2/MainActivity.java
@@ -1,8 +1,10 @@
 package itk.myoganugraha.ppb_minggu2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,24 +12,48 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button countUp, reset;
+    private Context mContext;
+    private Toast mtoast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+
 
         mShowCount = (TextView) findViewById(R.id.show_count);
+        countUp = (Button) findViewById(R.id.button_count);
+
+
+        countUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mCount++;
+                if(mShowCount != null)
+                    mShowCount.setText(Integer.toString(mCount));
+                showToast();
+            }
+        });
+
+        reset = (Button) findViewById(R.id.button_reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCount = 0;
+                mShowCount.setText(Integer.toString(mCount));
+                showToast();
+            }
+        });
     }
 
-
-    public void showToast(View view){
-        Toast.makeText(this, "now count :  " + mCount, Toast.LENGTH_SHORT).show();
-
-    }
-
-    public void countUp(View view) {
-        mCount++;
-        if(mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+    private void showToast() {
+        if(mtoast != null){
+            mtoast.cancel();
+        }
+        mtoast = Toast.makeText(mContext, "now count :  " + mCount, Toast.LENGTH_SHORT);
+        mtoast.show();
     }
 }
